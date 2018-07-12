f6067b4a4efa53e288b8771bab23e89ed0d80ba5
b_chirva <bchirva@gmail.com>
2018-07-13 04:30:16 +0500

    Add Snake::draw

--- Game.cpp
+++ Game.cpp
@@ -4,7 +4,7 @@ int Game::exec()
 {
     m_Apple = std::make_shared<Apple>();
     m_Window.addDrawable(m_Apple);
-    //m_Window.addDrawable(m_Snake);
+    m_Window.addDrawable(m_Snake);
     //m_Window.addDrawable(m_Wall);
 
     m_GraphicThread = std::thread(std::bind(&Window::drawLoop, &m_Window));
--- Snake.cpp
+++ Snake.cpp
@@ -23,13 +23,62 @@ void Snake::turn(EDirection ADirection)
 void Snake::makeStep()
 {
     m_CurrentDirection = m_NextDirection;
-    Point Head(*(m_Points.cbegin()));
-    
+    Point Head(m_Points.front());
     Head.move(m_CurrentDirection, 1);    
     m_Points.push_front(Head);
 }
 
 void Snake::draw(sf::RenderWindow &ADrawingWindow) const
 {
-    (void)(ADrawingWindow);
+    for(auto point = m_Points.cbegin(); point != m_Points.cend(); ++point)
+    {
+        sf::Texture texture;
+        
+        if(point != m_Points.cbegin() && point != --m_Points.cend())
+        {
+            auto prev = point;
+            auto next = point;
+            prev--;
+            next++;
+
+            Point dNext(point->getX() - next->getX(), point->getY() - next->getY());
+            Point dPrev(point->getX() - prev->getX(), point->getY() - prev->getY());
+
+            if(     dNext.getX() != 0 && dPrev.getX() != 0)
+                texture = SpriteLoader::getSprite(ESprite::LineHorizontal);
+            else if(dNext.getY() != 0 && dPrev.getY() != 0)
+                texture = SpriteLoader::getSprite(ESprite::LineVertical);
+            else
+            {
+                Point dSide(dNext.getX() + dPrev.getX(), dNext.getY() + dPrev.getY());
+                if(     dSide.getX() == 1   && dSide.getY() == 1)
+                    texture = SpriteLoader::getSprite(ESprite::BottomRightCorner);
+                else if(dSide.getX() == 1   && dSide.getY() == -1)
+                    texture = SpriteLoader::getSprite(ESprite::TopRightCorner);
+                else if(dSide.getX() == -1  && dSide.getY() == 1)
+                    texture = SpriteLoader::getSprite(ESprite::BottomLeftCorner);
+                else if(dSide.getX() == -1  && dSide.getY() == -1)
+                    texture = SpriteLoader::getSprite(ESprite::TopLeftCorner);
+            }
+        }
+        else
+        {
+            auto side = point;
+            if(point == m_Points.cbegin())
+                side++;
+            if(point == --m_Points.cend())
+                side--;
+
+            Point dSide(point->getX() - side->getX(), point->getY() - side->getY());
+            if(dSide.getY() != 0)
+                texture = SpriteLoader::getSprite(ESprite::LineVertical);
+            else if(dSide.getX() != 0)
+                texture = SpriteLoader::getSprite(ESprite::LineHorizontal);
+        }
+
+        sf::Sprite sprite(texture);
+        sprite.setColor(sf::Color::Green);
+        sprite.setPosition(point->getX(), point->getY());
+        ADrawingWindow.draw(sprite);
+    }
 }
