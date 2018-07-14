eb5b9d70f8ca492897c7d472e290537e9060dd60
b_chirva <bchirva@gmail.com>
2018-07-14 07:29:09 +0500

    Forward fill Line

--- Line.cpp
+++ Line.cpp
@@ -2,11 +2,11 @@
 
 Line::Line(Point ABegin, EDirection ADirection, uint8_t ALength)
 {
-    m_Points.push_back(ABegin);
+    m_Points.push_front(ABegin);
     for(int i = 0; i < ALength; i++)
     {
         ABegin.move(ADirection, 1);
-        m_Points.push_back(ABegin);
+        m_Points.push_front(ABegin);
     }
 }
 
