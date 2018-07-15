06b1bd4a43676ecf2598064b1dddda249a0465d0
b_chirva <bchirva@gmail.com>
2018-07-15 09:41:13 +0500

    Feat: Wall::draw()

--- Wall.cpp
+++ Wall.cpp
@@ -2,5 +2,33 @@
 
 void Wall::draw(sf::RenderWindow& ADrawingWindow) const
 {
-    (void)(ADrawingWindow);
+    for (auto point: m_Points)
+    {
+        sf::Texture texture;
+        sf::Sprite sprite;
+
+        bool isTop      = isHit(Point(point.getX(),     point.getY() - 1));
+        bool isBottom   = isHit(Point(point.getX(),     point.getY() + 1));
+        bool isLeft     = isHit(Point(point.getX() - 1, point.getY()));
+        bool isRight    = isHit(Point(point.getX() + 1, point.getY()));
+        ESprite TargetTexture;
+
+        if (isTop && isBottom && !isLeft && !isRight)           TargetTexture = ESprite::LineVertical;
+        else if (isLeft && isRight && !isTop && !isBottom)      TargetTexture = ESprite::LineHorizontal;
+        else if (isLeft && isRight && isTop && !isBottom)       TargetTexture = ESprite::TopHalfCross;
+        else if (isLeft && isRight && !isTop && isBottom)       TargetTexture = ESprite::BottomHalfCross;
+        else if (isLeft && !isRight && isTop && isBottom)       TargetTexture = ESprite::LeftHalfCross;
+        else if (!isLeft && isRight && isTop && isBottom)       TargetTexture = ESprite::RightHalfCross;
+        else if (isLeft && !isRight && isTop && !isBottom)      TargetTexture = ESprite::TopLeftCorner;
+        else if (isLeft && !isRight && !isTop && isBottom)      TargetTexture = ESprite::BottomLeftCorner;
+        else if (!isLeft && isRight && isTop && !isBottom)      TargetTexture = ESprite::TopRightCorner;
+        else if (!isLeft && isRight && !isTop && isBottom)      TargetTexture = ESprite::BottomRightCorner;
+        else /*if (isLeft && isRight && isTop && isBottom)*/    TargetTexture = ESprite::LineCross;
+
+        texture = SpriteLoader::getSprite(TargetTexture);
+        sprite.setTexture(texture);
+        sprite.setColor(sf::Color(128, 128, 128));
+        sprite.setPosition(point.getX() * 16, point.getY() * 16);
+        ADrawingWindow.draw(sprite);
+    } 
 }
